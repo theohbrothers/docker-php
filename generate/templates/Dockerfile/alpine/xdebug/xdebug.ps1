70edaa5e65f8e5337e43080d1b16e52b1056adb8
Leo <leonardjonathanoh@live.com>
2018-11-09 17:55:42 +0800

    trigger rebuild for smaller xdebug variants

--- generate/templates/Dockerfile/alpine/xdebug/xdebug.ps1
+++ generate/templates/Dockerfile/alpine/xdebug/xdebug.ps1
@@ -1,9 +1,9 @@
 @'
 # Xdebug: https://stackoverflow.com/questions/46825502/how-do-i-install-xdebug-on-dockers-official-php-fpm-alpine-image
 # PHPIZE_DEPS: autoconf dpkg-dev dpkg file g++ gcc libc-dev make pkgconf re2c
-RUN apk add --no-cache $PHPIZE_DEPS \
+RUN apk add --no-cache --virtual .build-dependencies $PHPIZE_DEPS \
     && pecl install xdebug-2.6.0 \
     && docker-php-ext-enable xdebug \
     && docker-php-source delete \
-    && apk del --purge autoconf g++ make
+    && apk del .build-dependencies
 '@
