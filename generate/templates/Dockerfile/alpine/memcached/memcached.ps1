17b39d7251e80cbaf509fd0355c38a7d22b3b70d
Leo <leonardjonathanoh@live.com>
2019-03-06 15:56:10 +0800

    cleanup the Dockerfile further for memcache fragment

--- generate/templates/Dockerfile/alpine/memcached/memcached.ps1
+++ generate/templates/Dockerfile/alpine/memcached/memcached.ps1
@@ -1,9 +1,9 @@
 @'
 # memcached
 # See: https://stackoverflow.com/questions/40894385/how-can-i-install-the-php-memcached-extension-on-dockers-php7-alpine-image
-RUN apk add --no-cache --update libmemcached-libs zlib \
-    && apk add --no-cache --update --virtual .phpize-deps $PHPIZE_DEPS \
-    && apk add --no-cache --update --virtual .memcached-deps zlib-dev libmemcached-dev cyrus-sasl-dev \
+RUN apk add --no-cache libmemcached-libs zlib \
+    && apk add --no-cache --virtual .phpize-deps $PHPIZE_DEPS \
+    && apk add --no-cache --virtual .memcached-deps zlib-dev libmemcached-dev cyrus-sasl-dev \
     && pecl install memcached \
     && echo "extension=memcached.so" > /usr/local/etc/php/conf.d/20_memcached.ini \
     && docker-php-source delete \
