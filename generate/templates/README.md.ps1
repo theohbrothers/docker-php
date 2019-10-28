85ce40745b8dd036092e54f831bf2947cc4f0b23
Leo <leonardjonathanoh@live.com>
2019-10-28 18:27:48 +0000

    Update readme with docker-image-layers badge

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -4,7 +4,7 @@
 [![gitlab-ci](https://img.shields.io/gitlab/pipeline/leojonathanoh/docker-php-fpm-alpine/dev)](https://gitlab.com/leojonathanoh/docker-php-fpm-alpine/commits/dev)
 [![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-php-fpm-alpine)](https://github.com/leojonathanoh/docker-php-fpm-alpine/releases/)
 [![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-php-fpm-alpine/latest)](https://hub.docker.com/r/leojonathanoh/docker-php-fpm-alpine)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-php-fpm-alpine/latest)](https://hub.docker.com/r/leojonathanoh/docker-php-fpm-alpine)
+[![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-php-fpm-alpine/latest)](https://hub.docker.com/r/leojonathanoh/docker-php-fpm-alpine)
 
 Official php:7.2-fpm-alpine with php extension(s).
 
