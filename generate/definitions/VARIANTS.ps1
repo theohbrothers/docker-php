71683b610514876bdc041e778443aa1ae6f61564
Leo <leonardjonathanoh@live.com>
2020-03-25 04:20:17 +0000

    Improve Variant templating; Regenerate Dockerfiles

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -1,64 +1,47 @@
 # Docker image variants' definitions
+$local:VARIANTS_MATRIX = @(
+    @{
+        base_image_tag = '7.2-fpm-alpine3.10'
+        subvariants = @(
+            @{ components = @( 'opcache' ) }
+            @{ components = @( 'mysqli' ) }
+            @{ components = @( 'gd' ) }
+            @{ components = @( 'pdo' ) }
+            @{ components = @( 'memcached' ) }
+            @{ components = @( 'sockets' ) }
+            @{ components = @( 'xdebug' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'pdo' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'pdo', 'memcached' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'pdo', 'memcached', 'sockets' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'xdebug' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'pdo', 'xdebug' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'pdo', 'memcached', 'xdebug' ) }
+            @{ components = @( 'opcache', 'mysqli', 'gd', 'pdo', 'memcached', 'sockets', 'xdebug' ) }
+        )
+    }
+)
 $VARIANTS = @(
-    @{
-        tag = 'opcache'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'mysqli'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'gd'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'pdo'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'memcached'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'sockets'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'xdebug'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-pdo'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-pdo-memcached'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-pdo-memcached-sockets'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-xdebug'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-pdo-xdebug'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-pdo-memcached-xdebug'
-        distro = 'alpine'
-    }
-    @{
-        tag = 'opcache-mysqli-gd-pdo-memcached-sockets-xdebug'
-        distro = 'alpine'
+    foreach ($variant in $VARIANTS_MATRIX){
+        foreach ($subVariant in $variant['subvariants']) {
+            @{
+                # Metadata object
+                _metadata = @{
+                    base_image_tag = $variant['base_image_tag']
+                    components = $subVariant['components']
+                }
+                # Docker image tag. E.g. '7.2-fpm-alpine3.10-opcache', '7.2-fpm-alpine3.10-mysqli'
+                tag = @(
+                    $variant['base_image_tag']
+                    $subVariant['components'] | ? { $_ }
+                ) -join '-'
+                tag_as_latest = if ( $subVariant.Contains('tag_as_latest') ) {
+                                    $subVariant['tag_as_latest']
+                                } else {
+                                    $false
+                                }
+            }
+        }
     }
 )
 
@@ -67,7 +50,7 @@ $VARIANTS_SHARED = @{
     buildContextFiles = @{
         templates = @{
             'Dockerfile' = @{
-                common = $false
+                common = $true
                 includeHeader = $true
                 includeFooter = $true
                 passes = @(
