a114c92c96c39523980fbe86fc29ad3423a10288
Leo <leonardjonathanoh@live.com>
2020-03-25 04:46:40 +0000

    Update readme on image description

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -6,7 +6,7 @@
 [![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-php-fpm-alpine/latest)](https://hub.docker.com/r/theohbrothers/docker-php-fpm-alpine)
 [![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-php-fpm-alpine/latest)](https://hub.docker.com/r/theohbrothers/docker-php-fpm-alpine)
 
-Official php:7.2-fpm-alpine with php extension(s).
+Dockerized php with php extension(s), based on [official php images](https://hub.docker.com/_/php).
 
 # Variants
 
