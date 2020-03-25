8ff286d15202dfca8f0fb78db5b7c7675acebdfa
Leo <leonardjonathanoh@live.com>
2020-03-25 04:40:20 +0000

    Attempt to fix xdebug installation failures on php 7.3 and 7.4

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -88,7 +88,7 @@ RUN docker-php-ext-install sockets
 # Xdebug: https://stackoverflow.com/questions/46825502/how-do-i-install-xdebug-on-dockers-official-php-fpm-alpine-image
 # PHPIZE_DEPS: autoconf dpkg-dev dpkg file g++ gcc libc-dev make pkgconf re2c
 RUN apk add --no-cache --virtual .build-dependencies $PHPIZE_DEPS \
-    && pecl install xdebug-2.6.0 \
+    && pecl install xdebug \
     && docker-php-ext-enable xdebug \
     && docker-php-source delete \
     && apk del .build-dependencies
