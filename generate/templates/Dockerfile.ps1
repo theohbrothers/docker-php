18455fe2b21b57e4c937299d05a0bbc75046e0c3
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-01 14:58:34 +0000

    Fix 8.0 variants with gd

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -8,7 +8,7 @@ $(
         'gd' {
             # Fix for php 7.4
             # See: https://github.com/docker-library/php/issues/931#issuecomment-568658449 and https://github.com/docker-library/php/issues/912#issuecomment-559918036
-            if ( $VARIANT['tag'] -match '^7.4') {
+            if ( $VARIANT['tag'] -match '^8.0|^7.4') {
                 @'
 # gd
 RUN apk add --no-cache freetype libpng libjpeg-turbo freetype-dev libpng-dev libjpeg-turbo-dev \
