39b36c6373d9c018e234ff0a1ea217a6921e46bf
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-02 08:27:54 +0000

    Bump xdebug to xdebug-3.0.2 for php 8.0 variants

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -107,7 +107,7 @@ RUN docker-php-ext-install sockets
 # Xdebug: https://stackoverflow.com/questions/46825502/how-do-i-install-xdebug-on-dockers-official-php-fpm-alpine-image
 # PHPIZE_DEPS: autoconf dpkg-dev dpkg file g++ gcc libc-dev make pkgconf re2c
 RUN apk add --no-cache --virtual .build-dependencies $PHPIZE_DEPS \
-    && pecl install xdebug-3.0.1 \
+    && pecl install xdebug-3.0.2 \
     && docker-php-ext-enable xdebug \
     && docker-php-source delete \
     && apk del .build-dependencies
