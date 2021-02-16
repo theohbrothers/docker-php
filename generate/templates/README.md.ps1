a87be9d0f1e9c7b23dade4fd7839a8b45b8e1131
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-16 15:15:48 +0000

    Docs: Replace microbadger image size and image layers badges with shields.io docker image size badge in readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -3,8 +3,7 @@
 
 [![github-actions](https://github.com/theohbrothers/docker-php/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-php/actions)
 [![github-tag](https://img.shields.io/github/tag/theohbrothers/docker-php)](https://github.com/theohbrothers/docker-php/releases/)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-php/latest)](https://hub.docker.com/r/theohbrothers/docker-php)
-[![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-php/latest)](https://hub.docker.com/r/theohbrothers/docker-php)
+[![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-php/latest)](https://hub.docker.com/r/theohbrothers/docker-php)
 
 Dockerized php with php extension(s), based on [official php images](https://hub.docker.com/_/php).
 
