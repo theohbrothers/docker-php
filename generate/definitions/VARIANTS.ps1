0cb0ca515112335dfc96321d510f618f986a088e
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-04-13 13:44:30 +0000

    Feature: Add `8.1.17` variant

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -1,6 +1,7 @@
 # Docker image variants' definitions
 # See: https://www.php.net/releases/index.php?json&max=100&version=8.0
 $local:VARIANTS_BASE_IMAGE_TAGS = @(
+    '8.1.17-fpm-alpine3.17'
     '8.0.28-fpm-alpine3.16'
     '7.4.33-fpm-alpine3.16'
     '7.3.33-fpm-alpine3.15'
